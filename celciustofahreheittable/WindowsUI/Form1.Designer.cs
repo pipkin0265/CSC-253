794b04ed3fd7206b71780ed194a0d23dd7629867
Michael Pipkin II <71162077+pipkin0265@users.noreply.github.com>
2021-08-27 18:40:26 -0400

    Add files via upload
    Create an application that displays a table of the Celsius temperatures 0-20 and their Fahrenheit equivalents. The application should use a loop to display the temperatures in a list box.

---
+++ celciustofahreheittable/WindowsUI/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace WindowsUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.Location = new System.Drawing.Point(23, 12);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(225, 277);
+            this.lstBox.TabIndex = 0;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(93, 309);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(95, 35);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 365);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lstBox);
+            this.Name = "Form1";
+            this.Text = "Celcius To Fahreheit Table";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ celciustofahreheittable/WindowsUI/Form1.cs
@@ -0,0 +1,52 @@
+﻿/**
+ *08/18/2021
+ *CSC 253
+ *Michael Pipkin
+ *Create an application that displays a table of the Celsius temperatures
+ *0-20 and their Fahrenheit equivalents. The application should use loop to 
+ *display the temperatures in a list box.
+ **/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            // declaring variables
+            double cel = 0.0;
+            double fah = 0.0;
+
+            // clear list box
+            lstBox.Items.Clear();
+            lstBox.Items.Add("Celsius\t\tFahrenheit");
+
+            // using while loop
+            while (cel <= 20)
+            {
+                // convert temperature in to Fahrenheit
+                fah = (9 * cel / 5) + 32;
+
+                // add Celsius and Fahrenheit temperature into lstBox
+                lstBox.Items.Add(cel.ToString() + "\t\t" + fah.ToString());
+                
+                // increment value of celsius
+                cel++;
+            }
+        }
+    }
+}
